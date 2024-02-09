e7b8b7d2d4a0f57827a06cb4f6bf3b41d219796f
veeresh <veereshveer33@gmail.com>
2024-02-09 18:15:29 +0530

    swapping data file

---
+++ src/swappingData.java
@@ -0,0 +1,40 @@
+
+public class swappingData {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c;
+	
+		
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+	
+		
+		
+		
+		
+		
+//		a=a+b;
+//		b=a-b;
+//		a=a-b;
+//		System.out.println(a);
+//		System.out.println(b);
+//		
+//	//by using c variable
+//		c=a+b;
+//		b=c-a;
+//		a=c-b;
+//		System.out.println(a);
+//		System.out.println(b);
+		
+	
+		
+		
+
+	}
+
+}
